66ff1098ee6192cc603f776eb7c98219f2dad448
Jordan <Jordan.Brockopp@target.com>
2020-11-02 10:25:17 -0600

    test: upgrade docker/docker dependency

--- runtime/docker/container.go
+++ runtime/docker/container.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io"
 	"strings"
-	"time"
 
 	"github.com/go-vela/types/constants"
 
@@ -206,9 +205,6 @@ func (c *client) TailContainer(ctx context.Context, ctn *pipeline.Container) (io
 
 	// capture all stdout and stderr logs
 	go func() {
-		// sleep for 1 second
-		time.Sleep(1 * time.Second)
-
 		// create options for capturing container logs
 		//
 		// https://godoc.org/github.com/docker/docker/api/types#ContainerLogsOptions
