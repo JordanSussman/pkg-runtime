ff9a7963844acea4ad0b0e732263e6e864f1f834
Jordan <Jordan.Brockopp@target.com>
2020-11-04 14:05:31 -0600

    test: add loop to check container status

--- runtime/docker/container.go
+++ runtime/docker/container.go
@@ -198,38 +198,61 @@ func (c *client) SetupContainer(ctx context.Context, ctn *pipeline.Container) er
 
 // TailContainer captures the logs for the pipeline container.
 func (c *client) TailContainer(ctx context.Context, ctn *pipeline.Container) (io.ReadCloser, error) {
+	for {
+		// send API call to inspect the container
+		//
+		// https://godoc.org/github.com/docker/docker/client#Client.ContainerInspect
+		container, err := c.docker.ContainerInspect(ctx, ctn.ID)
+		if err != nil {
+			logrus.Errorf("unable to inspect container %s", ctn.ID)
+		}
+
+		// check if the container is in a running or exited status
+		//
+		// https://godoc.org/github.com/docker/docker/api/types#ContainerState
+		if strings.EqualFold(container.State.Status, "running") ||
+			strings.EqualFold(container.State.Status, "exited") {
+			// break so we start tailing container logs
+			break
+		}
+
+		logrus.Tracef("container %s is in a %s state; skipping tail of output", ctn.ID, container.State.Status)
+	}
+
 	logrus.Tracef("tailing output for container %s", ctn.ID)
+
+	// create options for capturing container logs
+	//
+	// https://godoc.org/github.com/docker/docker/api/types#ContainerLogsOptions
+	opts := types.ContainerLogsOptions{
+		Follow:     true,
+		ShowStdout: true,
+		ShowStderr: true,
+		Details:    false,
+		Timestamps: false,
+	}
+
+	// send API call to capture the container logs
+	//
+	// https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
+	logs, err := c.docker.ContainerLogs(ctx, ctn.ID, opts)
+	if err != nil {
+		return nil, err
+	}
 
 	// create in-memory pipe for capturing logs
 	rc, wc := io.Pipe()
 
+	logrus.Tracef("copying logs for container %s", ctn.ID)
+
 	// capture all stdout and stderr logs
 	go func() {
-		// create options for capturing container logs
-		//
-		// https://godoc.org/github.com/docker/docker/api/types#ContainerLogsOptions
-		opts := types.ContainerLogsOptions{
-			Follow:     true,
-			ShowStdout: true,
-			ShowStderr: true,
-			Details:    false,
-			Timestamps: false,
-		}
-
-		// send API call to capture the container logs
-		//
-		// https://godoc.org/github.com/docker/docker/client#Client.ContainerLogs
-		logs, err := c.docker.ContainerLogs(ctx, ctn.ID, opts)
-		if err != nil {
-			logrus.Errorf("unable to tail output for container: %v", err)
-		}
-
 		logrus.Tracef("copying logs for container %s", ctn.ID)
 
 		// copy container stdout and stderr logs to our in-memory pipe
 		//
 		// https://godoc.org/github.com/docker/docker/pkg/stdcopy#StdCopy
-		_, err = stdcopy.StdCopy(wc, wc, logs)
+		_, err := stdcopy.StdCopy(wc, wc, logs)
 		if err != nil {
 			logrus.Errorf("unable to copy logs for container: %v", err)
 		}
